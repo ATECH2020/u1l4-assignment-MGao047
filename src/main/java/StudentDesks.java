2149f80847d9bc46526982abb1eb4d18a5a61963
Mark Gao <mark.184748@nv.ccsd.net>
2020-09-07 00:56:41 +0000

    Code

--- src/main/java/StudentDesks.java
+++ src/main/java/StudentDesks.java
@@ -7,11 +7,18 @@ class StudentDesks {
         Scanner scanner = new Scanner(System.in);
 
         //Reads ints from user
-        
+        int userint1= scanner.nextInt();
+        int userint2= scanner.nextInt();
+        int userint3=scanner.nextInt();
+        System.out.println("Number of students in classroom 1:");
+        System.out.println("Number of students in classroom 2:");
+        System.out.println("Number of students in classroom 3: ");
+
 
         /*
          *  your code goes here
          */
+         System.out.println("Total desks needed for all classrooms:"+((userInt1/2)+userInt1 %2) + (userInt2/2 + userInt2 %2)+ (userInt3/2 + userInt3 %2));
 
 
         // closing the scanner object
