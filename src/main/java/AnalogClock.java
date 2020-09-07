d9a6a0f803460a6348d3a01620289f3c391e52ff
Mark Gao <mark.184748@nv.ccsd.net>
2020-09-07 05:31:29 +0000

    Improv

--- src/main/java/AnalogClock.java
+++ src/main/java/AnalogClock.java
@@ -2,29 +2,23 @@ import java.util.Scanner;
 
 class AnalogClock {
   public static void main(String[] args) {
-      int hour;
-      double minuteNumber;
-      double minute;
+      
 
         //Creates a new scanner for user input
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Degrees of hours: ");
-        double userDouble = scanner.nextDouble();
-
+        
         //Reads ints from user
-
+       int hourHand=scanner.nextInt();
 
         /*
          *  your code goes here
-          */
-          hour=(int)userDouble/30;
-          minuteNumber=((userDouble/30)-hour);
-          minute=minuteNumber*60;
-          if(minute-(int)minute>=.5);
-          {
-            minute =(int)minute+1;
-          }
-          System.out.println("Minute:"+minute);
+         */
+      int hour = hourHand /30;
+      int minutePass= hour * 30;
+      int minpass=hourHand - minutePass;
+      int minute = (30 - minutePass)*6;
+      System.out.print(minute);
+
 
         // closing the scanner object
         scanner.close();
